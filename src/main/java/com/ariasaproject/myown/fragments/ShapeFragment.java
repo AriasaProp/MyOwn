b3d9c75d901ec008e4d0e6ce6d8b6e2d15b518ed
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-09-30 14:19:54 +0800

    to-fragment : 14:19:54 09/30/24

--- src/main/java/com/ariasaproject/myown/fragments/ShapeFragment.java
+++ src/main/java/com/ariasaproject/myown/fragments/ShapeFragment.java
@@ -22,6 +22,17 @@ public class ShapeFragment extends Fragment {
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
+        btnSelect.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(
+                        	context,
+                                        "You selected: " + shapeSelector.getSelectedShape(),
+                                        Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
     }
 
     @Override
@@ -38,17 +49,7 @@ public class ShapeFragment extends Fragment {
         ViewGroup v = (ViewGroup) inflater.inflate(R.layout.fragment_shape, container, false);
         shapeSelector = (ShapeSelectorView) v.findViewById(R.id.shapeSelector);
         btnSelect = (AppCompatButton) v.findViewById(R.id.btnSelect);
-        btnSelect.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Toast.makeText(
-                                        ShapeDemoActivity.this,
-                                        "You selected: " + shapeSelector.getSelectedShape(),
-                                        Toast.LENGTH_LONG)
-                                .show();
-                    }
-                });
+        
         return v;
     }
 
